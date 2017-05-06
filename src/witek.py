b52f60f94770b9a86fceb4038adf6be2b333ed30
pikpikpik <W.Oleszkiewicz@stud.elka.pw.edu.pl>
2017-05-06 19:24:53 +0200

    Modification of number of epochs and conv. padding

--- src/witek.py
+++ src/witek.py
@@ -26,21 +26,21 @@ y_train = to_categorical(y_train, 10)
 y_test = to_categorical(y_test, 10)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 1), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 1), padding='valid', activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='valid', kernel_constraint=maxnorm(3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 1), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 1), padding='valid', activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='valid', kernel_constraint=maxnorm(3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
-epochs = 50
-lrate = 0.02
+epochs = 100
+lrate = 0.04
 decay = lrate/epochs
 sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
