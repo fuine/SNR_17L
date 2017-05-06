965c787e729f3e431c4f69109af5da72f1c27daa
Łukasz Neumann <fuine@riseup.net>
2017-05-06 19:12:39 +0200

    implementation in keras

--- src/conv.py
+++ src/conv.py
@@ -1,132 +1,176 @@
-import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import (
+    Dense,
+    Conv2D,
+    MaxPooling2D,
+    GaussianNoise,
+    Dropout,
+    Flatten
+)
+
+from keras.optimizers import SGD
+from keras.models import load_model
+from keras.callbacks import (
+    ReduceLROnPlateau,
+    ModelCheckpoint
+)
 
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from sklearn.utils import class_weight
+from sklearn.model_selection import train_test_split
 
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+# import pandas as pd
 import argparse
-
-import lasagne
-from lasagne import layers
-from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
-# from nolearn.lasagne.visualize import plot_conv_weights
+import itertools
+import os
 
 from utils import load_dataset
-from utils import balanced_subsample
+# from utils import balanced_subsample
 from utils import unison_shuffled_copies
 
-np.random.seed(42)
+from keras.utils import plot_model
+from resnet import ResnetBuilder
 
+# fix random seed for reproducibility
+np.random.seed(42)
 im_size = 32
 gray = True
+resnet = True
+verbosity = 2
+plot = True
+# To create validation set from train set set this parameter to the percentage
+# of the validation set (eg 0.2 will create a validation set from 20% of the
+# training set)
+valid_size = 0.1
+
+if gray:
+    channel_nums = 1
+else:
+    channel_nums = 3
 
 
-x_train, y_train = load_dataset('../data/train_32x32.mat', gray)
-x_test, y_test = load_dataset('../data/test_32x32.mat', gray)
-y_train = y_train.flatten()
-y_test = y_test.flatten()
-y_train[y_train == 10] = 0
-y_test[y_test == 10] = 0
-x_train = np.transpose(x_train, (0, 3, 1, 2))
-x_test = np.transpose(x_test, (0, 3, 1, 2))
+def probas_to_classes(proba):
+    """
+    Changes matrix of probabilities to array of predicted classes.
+    """
+    if proba.shape[-1] > 1:
+        return proba.argmax(axis=-1)
+    else:
+        return (proba > 0.5).astype('int32')
 
 
-# y_d = pd.Series(y_train)
-# subinds = select_classes(y_d, [1, 8])
-# x_train = x_train[subinds]
-# y_train = y_train[subinds]
-#
-# y_d = pd.Series(y_test)
-# subinds = select_classes(y_d, [1, 8])
-# x_test = x_test[subinds]
-# y_test = y_test[subinds]
+def plot_confusion_matrix(
+        cm, classes, normalize=False, title='Confusion matrix',
+        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes)
 
-print("Bincount in y_train before balancing: {}".format(np.bincount(y_train)))
-print("Bincount in y_test before balancing: {}".format(np.bincount(y_test)))
-y_d = pd.Series(y_train)
-inds = balanced_subsample(y_d)
-x_train = x_train[inds]
-y_train = y_train[inds]
-x_train, y_train = unison_shuffled_copies(x_train, y_train)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
 
-# y_train[y_train == 8] = 0
-# y_test[y_test == 8] = 0
-# print("First 10 labels in y_test: {}".format(y_train[:10]))
-# for i in range(10):
-#     plt.figure()
-#     # plt.imshow(x_train[i])  # , cmap='gray')
-#     plt.imshow(np.reshape(x_train[i], (im_size, im_size)), cmap='gray')
-# plt.show()
+    print(cm)
 
-print(x_train[0])
-print("x_train shape: {}".format(x_train.shape))
-print("x_train dtype: {}".format(x_train.dtype))
-print("y_train shape: {}".format(y_train.shape))
-print("x_test shape: {}".format(x_test.shape))
-print("x_test dtype: {}".format(x_test.dtype))
-print("y_test shape: {}".format(y_test.shape))
-print("Unique values in y_train: {}".format(np.unique(y_train)))
-print("Bincount in y_train: {}".format(np.bincount(y_train)))
-print("Bincount in y_test: {}".format(np.bincount(y_test)))
-print("First 10 labels in y_test: {}".format(y_test[:10]))
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
 
-# classic INPUT -> [CONV -> RELU -> POOL]*2 -> FC -> RELU -> FC
-# note that RELU layers are sewn into conv/dense layers by nonlinearity function
-net1 = NeuralNet(
-    layers=[('input', layers.InputLayer),
-            ('conv2d1', layers.Conv2DLayer),
-            ('maxpool1', layers.MaxPool2DLayer),
-            ('conv2d2', layers.Conv2DLayer),
-            ('maxpool2', layers.MaxPool2DLayer),
-            ('gaussian1', layers.GaussianNoiseLayer),
-            ('dense1', layers.DenseLayer),
-            ('dropout1', layers.DropoutLayer),
-            #  ('dense2', layers.DenseLayer),
-            #  ('dropout2', layers.DropoutLayer),
-            ('output', layers.DenseLayer),
-            ],
-    # input layer
-    input_shape=(None, 1, im_size, im_size),
-    # layer conv2d1
-    conv2d1_num_filters=32,
-    conv2d1_filter_size=(3, 3),
-    conv2d1_stride=1,
-    conv2d1_pad='same',
-    conv2d1_nonlinearity=lasagne.nonlinearities.rectify,
-    conv2d1_W=lasagne.init.GlorotUniform(),
-    # layer maxpool1
-    maxpool1_pool_size=(2, 2),
-    # layer conv2d2
-    conv2d2_num_filters=64,
-    conv2d2_filter_size=(3, 3),
-    conv2d2_stride=1,
-    conv2d2_pad='same',
-    conv2d2_nonlinearity=lasagne.nonlinearities.rectify,
-    # layer maxpool2
-    maxpool2_pool_size=(4, 4),
-    # gaussian1
-    gaussian1_sigma=0.2,
-    # dense
-    dense1_num_units=2048,
-    dense1_nonlinearity=lasagne.nonlinearities.rectify,
-    # dropout2
-    dropout1_p=0.5,
-    #  dense2_num_units=128,
-    #  dense2_nonlinearity=lasagne.nonlinearities.rectify,
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
-    #  dropout2_p=0.7,
-    # output
-    output_nonlinearity=lasagne.nonlinearities.softmax,
-    output_num_units=10,
-    # optimization hyperparameters
-    update=nesterov_momentum,
-    update_learning_rate=0.01,
-    update_momentum=0.9,
-    max_epochs=10,
-    verbose=1,
-)
+
+def plot_history(history):
+    """
+    Plots history from the given History object.
+    """
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.figure()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+
+
+def load_data():
+    x_train, y_train = load_dataset('../data/train_32x32.mat', gray)
+    x_test, y_test = load_dataset('../data/test_32x32.mat', gray)
+    y_train = y_train.flatten()
+    y_test = y_test.flatten()
+    y_train[y_train == 10] = 0
+    y_test[y_test == 10] = 0
+
+    # Shuffle train examples
+    x_train, y_train = unison_shuffled_copies(x_train, y_train)
+
+    print("x_train shape: {}".format(x_train.shape))
+    print("x_train dtype: {}".format(x_train.dtype))
+    print("y_train shape: {}".format(y_train.shape))
+    print("x_test shape: {}".format(x_test.shape))
+    print("x_test dtype: {}".format(x_test.dtype))
+    print("y_test shape: {}".format(y_test.shape))
+    print("Unique values in y_train: {}".format(np.unique(y_train)))
+    print("Bincount in y_train: {}".format(np.bincount(y_train)))
+    print("Bincount in y_test: {}".format(np.bincount(y_test)))
+    print("First 10 labels in y_test: {}".format(y_test[:10]))
+    y_train = np.reshape(y_train, (-1, 1))
+    y_test = np.reshape(y_test, (-1, 1))
+
+    return x_train, y_train, x_test, y_test
+
+
+def get_model(checkpoint_path=None):
+    # create model
+    if resnet:
+        model = ResnetBuilder.build_resnet_18((channel_nums, im_size, im_size),
+                                              10)
+    else:
+        model = Sequential([
+            Conv2D(filters=32, kernel_size=3, strides=1, padding="same",
+                   kernel_initializer='glorot_normal', activation='relu',
+                   input_shape=(im_size, im_size, channel_nums)),
+            MaxPooling2D(pool_size=2),
+            Conv2D(filters=64, kernel_size=3, strides=1, padding="same",
+                   activation='relu'),
+            MaxPooling2D(pool_size=4),
+            GaussianNoise(stddev=0.2),
+            Flatten(),
+            Dropout(rate=0.5),
+            Dense(units=2048, activation='relu'),
+            Dropout(rate=0.5),
+            Dense(units=10, activation='softmax')
+        ])
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=sgd,
+                  metrics=['accuracy'])
+    return model
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -136,38 +180,100 @@ if __name__ == "__main__":
         '-s', '--save', type=str,
         help='train and save trained network to the specified file')
     parser.add_argument(
-        '-v', '--visualize', type=argparse.FileType('rb'),
+        '-v', '--visualize', type=str,
         help='visualize trained network from the specified file')
+    parser.add_argument(
+        '-i', '--inc_train', type=str,
+        help='incrementally train the network with n epochs,'
+        ' requires -l argument, saves uptrained network to the given file'
+    )
+    parser.add_argument(
+        '-e', '--epochs', type=int,
+        default=10,
+        help='number of epochs, defaults to 10'
+    )
 
     args = parser.parse_args()
+
+    x_train, y_train, x_test, y_test = load_data()
+    class_weight = class_weight.compute_class_weight(
+        'balanced', np.unique(y_train.flatten()), y_train.flatten())
+    print(class_weight)
+
+    if valid_size is not None:
+        x_train, x_valid, y_train, y_valid = train_test_split(
+            x_train, y_train, test_size=valid_size,
+            random_state=42, stratify=y_train)
+        validation_data = (x_valid, y_valid)
+    else:
+        validation_data = None
+
+    callbacks = []
+    reduce_lr = ReduceLROnPlateau(
+        verbose=1, patience=3, min_lr=0.001)
+    callbacks.append(reduce_lr)
+
+    # TODO check if paths are valid before calculations
     if args.save:
         # Train the network
-        nn = net1.fit(x_train, y_train)
-        net1.save_params_to(args.save)
+        filename, file_extension = os.path.splitext(args.save)
+        model = get_model()
+        checkpoint = "{}_chk_{{epoch:02d}}-{{val_loss:.2f}}.{}".format(
+            filename, file_extension)
+        callbacks.append(ModelCheckpoint(checkpoint, verbose=1,
+                                         save_best_only=True, period=3))
+        print(model.summary())
+        history = model.fit(x_train, y_train, epochs=args.epochs,
+                            class_weight=class_weight,
+                            validation_data=validation_data,
+                            callbacks=callbacks,
+                            verbose=verbosity)
+        model.save(args.save)
+        if plot:
+            plot_history(history)
+    elif args.inc_train:
+        if args.load is None:
+            to_load = args.inc_train
+        else:
+            to_load = args.load
+        model = load_model(to_load)
+        filename, file_extension = os.path.splitext(args.inc_train)
+        checkpoint = "{}_chk_{{epoch:02d}}-{{val_loss:.2f}}{}".format(
+            filename, file_extension)
+        callbacks.append(ModelCheckpoint(checkpoint, verbose=1,
+                                         save_best_only=True, period=3))
+        print(model.summary())
+        history = model.fit(x_train, y_train, epochs=args.epochs,
+                            class_weight=class_weight,
+                            validation_data=validation_data,
+                            callbacks=callbacks,
+                            verbose=verbosity)
+        model.save(args.inc_train)
+        if plot:
+            plot_history(history)
     elif args.load:
         # Load the network
-        net1.load_params_from(args.load)
-        nn = net1
+        model = load_model(args.load)
+        print(model.summary())
     elif args.visualize:
-        pass
-        net1.load_params_from(args.load)
-        nn = net1
-        # plot_conv_weights(nn, figsize=(4, 4))
+        model = load_model(args.visualize)
+        plot_model(model, to_file='model.png', show_shapes=True)
         exit()
     else:
-        nn = net1.fit(x_train, y_train)
+        model = get_model()
+        print(model.summary())
+        history = model.fit(x_train, y_train, epochs=args.epochs,
+                            class_weight=class_weight,
+                            validation_data=validation_data,
+                            callbacks=callbacks,
+                            verbose=verbosity)
+        if plot:
+            plot_history(history)
 
-    # Classify test set
-    preds = nn.predict(x_test)
-    print(preds[:10])
-    # Print report
-    # print(y_test)
-    # print(preds)
+    probas = model.predict(x_test, verbose=verbosity)
+    preds = probas_to_classes(probas)
     print(classification_report(y_test, preds, digits=5))
     cm_ = confusion_matrix(y_test, preds)
-    plt.matshow(cm_)
-    plt.title('Confusion matrix')
-    plt.colorbar()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    plot_confusion_matrix(cm_, classes=np.unique(y_test).tolist(),
+                          title='Confusion matrix, without normalization')
     plt.show()
